b1e6e41337e176b5e87dffa144c2967bfc587de1
A G <Anubh896@gmail.com>
2023-10-10 22:09:44 +0530

    connection-issue

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ import jwt from "jsonwebtoken";
 import path from "path";
 import multer from "multer";
 import nodemailer from "nodemailer";
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import Stripe from "stripe";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-
-
 const app = express();
-const BUCKET_NAME = 'e-visa-images'
-const BUCKET_REGION = 'ap-south-1'
-const ACCESS_KEY ="AKIA2NYYZWF3GOP55BVU"
-const SECRET_ACCESS_KEY = "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"
-
+const BUCKET_NAME = "e-visa-images";
+const BUCKET_REGION = "ap-south-1";
+const ACCESS_KEY = "AKIA2NYYZWF3GOP55BVU";
+const SECRET_ACCESS_KEY = "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";
 
 const s3 = new S3Client({
-credentials:{
-  accessKeyId:ACCESS_KEY,
-  secretAccessKey:SECRET_ACCESS_KEY
-},
-region: BUCKET_REGION
-})
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+  region: BUCKET_REGION,
+});
 
 app.use(express.urlencoded({ extended: true }));
 const stripe = new Stripe(
@@ -56,7 +57,13 @@ const con = mysql.createConnection({
   password: "Evisa@9844#",
   database: "u281849479_visaform",
 });
-
+const db_config = {
+  host: "srv1115.hstgr.io",
+  port: 3306,
+  user: "u281849479_visaform",
+  password: "Evisa@9844#",
+  database: "u281849479_visaform",
+}
 // const con = mysql.createConnection({
 //   host: "localhost",
 //   port: 3306,
@@ -85,7 +92,7 @@ var transporter = nodemailer.createTransport({
 var mailOptions = {
   from: "info@indiaevisaservices.org",
   to: "info@indiaevisaservices.org",
-  cc:"info@indiaevisaservices.org",
+  cc: "info@indiaevisaservices.org",
   subject: "Sending Email test",
   html: `<!DOCTYPE html>
   <html lang="en">
@@ -124,10 +131,23 @@ var mailOptions = {
   </html> 
 `,
 };
+function handleDisconnect() {
+  console.log("handleDisconnect()");
+  connection.destroy();
+  connection = mysql.createConnection(db_config);
+  connection.connect(function (err) {
+    if (err) {
+      console.log(" Error when connecting to db  (DBERR001):", err);
+      setTimeout(handleDisconnect, 1000);
+    }
+  });
+}
 
 con.connect(function (err) {
   if (err) {
-    console.log("Error in Connection", err);
+    console.log("Connection is asleep (time to wake it up): ", err);
+    setTimeout(handleDisconnect, 1000);
+    handleDisconnect();
   } else {
     console.log("Connected");
   }
@@ -152,7 +172,10 @@ app.post("/checkout", async (req, res) => {
     const values = ["Completed", paymentIntent.id, "card", req.body.tempId];
     con.query(sql, values, (err, result) => {
       console.log(sql, values);
-      if (err) return res.json({ Error: err });
+      if (err) {
+        setTimeout(handleDisconnect, 1000);
+        handleDisconnect();
+      }
       if (result) {
         var mailOptions = {
           from: "info@indiaevisaservices.org",
@@ -223,14 +246,22 @@ app.post("/checkout", async (req, res) => {
                       <p>Thank you for submitting your application for the India eVisa. We are pleased to inform you that your application has been successfully processed and submitted for assessment. Our team aims to approve all applications within 24-48 hours. Once your application has been approved, you will receive an email from the Indian Immigration Authorities confirming your India eVisa approval.</p>
                   </div>
                   <div class="transaction-details">
-                      <p><strong>Transaction ID:</strong> ${paymentIntent.id}</p>
+                      <p><strong>Transaction ID:</strong> ${
+                        paymentIntent.id
+                      }</p>
                       <p><strong>Transaction Date:</strong> ${new Date()}</p>
-                      <p><strong>Temporary Application Number (not for eVisa status tracking):</strong> ${req.body.tempId}</p>
+                      <p><strong>Temporary Application Number (not for eVisa status tracking):</strong> ${
+                        req.body.tempId
+                      }</p>
                       <p><strong>Item 1:</strong> X EVISA INDIA</p>
-                      <p><strong>Cost:</strong> $${req.body.amount/100} USD</p>
+                      <p><strong>Cost:</strong> $${
+                        req.body.amount / 100
+                      } USD</p>
                       <p><strong>Charges on your card will appear as:</strong> India Evisa Services</p>
                   </div>
-                  <a href="https://master--iridescent-fox-f31d24.netlify.app/details/${req.body.tempId}" class="action-button">Complete Application</a>
+                  <a href="https://master--iridescent-fox-f31d24.netlify.app/details/${
+                    req.body.tempId
+                  }" class="action-button">Complete Application</a>
                   <div class="footer">
                       <p>If you did not authorize this transaction, please inform us by replying to this email.</p>
                       <p>If you have not completed your application yet, please click on the "Complete Application" button as soon as possible to ensure a prompt processing time.</p>
@@ -262,44 +293,11 @@ app.post("/checkout", async (req, res) => {
     console.log(err);
   }
 });
-app.get("/logout", (req, res) => {
-  res.clearCookie("token");
-  return res.json({ Status: "Success" });
-});
 
 app.get("/getip", (req, res) => {
   const ipAddress = req.ip; // Client's IP address
   res.json({ ip: ipAddress });
 });
-
-app.get("/employeeCount", (req, res) => {
-  const sql = "Select count(id) as employee from employee";
-  con.query(sql, (err, result) => {
-    if (err) return res.json({ Error: "Err" });
-    return res.json(result);
-  });
-});
-app.get("/salarySum", (req, res) => {
-  const sql = "Select sum(salary) sumOfSalary from employee";
-  con.query(sql, (err, result) => {
-    if (err) return res.json({ Error: "Err" });
-    return res.json(result);
-  });
-});
-const verifyUser = (req, res, next) => {
-  const token = req.cookies.token;
-  console.log(token);
-  if (!token) {
-    return res.json({ Error: " You are not Authenticated" });
-  } else {
-    jwt.verify(token, "jwt-secret", (err, decoded) => {
-      if (err) return res.json({ Error: "Wrong token" });
-      req.role = decoded.role;
-      req.id = decoded.id;
-      next();
-    });
-  }
-};
 
 app.get("/dashboard", verifyUser, (req, res) => {
   return res.json({ Status: "Success", role: req.role, id: req.id });
@@ -309,7 +307,10 @@ app.get("/tempId/:id", (req, res) => {
   const sql = "SELECT * from customer where TempId = ?";
   const id = req.params.id;
   con.query(sql, [id], (err, result) => {
-    if (err) return res.json({ Error: "Error!" });
+    if (err) {
+      setTimeout(handleDisconnect, 1000);
+      handleDisconnect();
+    }
     return res.json({ Status: "Success", Result: result });
   });
 });
@@ -319,7 +320,10 @@ app.get("/getLeads", async (req, res) => {
     "SELECT customer.*, passportdetails.*, otherdetails.*, paymentdetails.* FROM customer INNER JOIN passportdetails ON customer.TempId = passportdetails.id INNER JOIN otherdetails ON customer.TempId = otherdetails.id  INNER JOIN paymentdetails ON customer.TempId = paymentdetails.id";
   con.query(sql, (err, result) => {
     console.log("call");
-    if (err) return res.json({ Error: err });
+    if (err) {
+      setTimeout(handleDisconnect, 1000);
+      handleDisconnect();
+    }
     console.log("result");
     return res.json({ Status: "Success", result: result });
   });
@@ -559,78 +563,84 @@ LEFT JOIN
    paymentdetails ON customer.TempId = paymentdetails.id
 WHERE
   customer.TempId = ?`;
-  con.query(sql, [id], async(err, result) => {
+  con.query(sql, [id], async (err, result) => {
     if (err) throw err;
     for (const res of result) {
-      if(res.applicantFile){
-      const getObjectParams = {
-        Bucket: BUCKET_NAME,
-        Key: res.applicantFile,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const url = await getSignedUrl(s3, command, { expiresIn: 36000});
-      res.applicantUrl = url;
+      if (res.applicantFile) {
+        const getObjectParams = {
+          Bucket: BUCKET_NAME,
+          Key: res.applicantFile,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, { expiresIn: 36000 });
+        res.applicantUrl = url;
+      }
     }
-  }
     for (const res of result) {
-      if(res.passportFile){
-      const getObjectParams = {
-        Bucket: BUCKET_NAME,
-        Key: res.passportFile,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const url = await getSignedUrl(s3, command, { expiresIn: 36000});
-      res.passportUrl = url;
+      if (res.passportFile) {
+        const getObjectParams = {
+          Bucket: BUCKET_NAME,
+          Key: res.passportFile,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, { expiresIn: 36000 });
+        res.passportUrl = url;
+      }
     }
-  }
     for (const res of result) {
-      if(res.businessFile){
-      const getObjectParams = {
-        Bucket: BUCKET_NAME,
-        Key: res.businessFile,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const url = await getSignedUrl(s3, command, { expiresIn: 36000});
-      res.businessUrl = url;
+      if (res.businessFile) {
+        const getObjectParams = {
+          Bucket: BUCKET_NAME,
+          Key: res.businessFile,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, { expiresIn: 36000 });
+        res.businessUrl = url;
+      }
     }
-  }
     return res.json({ message: "Success", data: result });
   });
 });
 
-app.put("/otherDetails", upload.fields(uploadFields), async(req, res) => {
-  const applicantImageName = Math.floor(Math.random() * 1000000000) + req.files["applicantFile"]?.[0].originalname
-  const businessImageName = req.files["businessFile"] ? Math.floor(Math.random() * 1000000000) + req.files["businessFile"]?.[0]?.originalname : null
-  const passportImageName = Math.floor(Math.random() * 1000000000) + req.files["applicantFile"]?.[0]?.originalname
+app.put("/otherDetails", upload.fields(uploadFields), async (req, res) => {
+  const applicantImageName =
+    Math.floor(Math.random() * 1000000000) +
+    req.files["applicantFile"]?.[0].originalname;
+  const businessImageName = req.files["businessFile"]
+    ? Math.floor(Math.random() * 1000000000) +
+      req.files["businessFile"]?.[0]?.originalname
+    : null;
+  const passportImageName =
+    Math.floor(Math.random() * 1000000000) +
+    req.files["applicantFile"]?.[0]?.originalname;
   const params1 = {
     Bucket: BUCKET_NAME,
     Key: applicantImageName,
-    Body:req.files["applicantFile"]?.[0].buffer,
-    ContentType:req.files["applicantFile"]?.[0].mimetype
-   }
-   const command1 = new PutObjectCommand(params1)
-   await s3.send(command1)
+    Body: req.files["applicantFile"]?.[0].buffer,
+    ContentType: req.files["applicantFile"]?.[0].mimetype,
+  };
+  const command1 = new PutObjectCommand(params1);
+  await s3.send(command1);
 
-   const params2 = {
+  const params2 = {
     Bucket: BUCKET_NAME,
     Key: businessImageName,
-    Body:req.files["businessFile"]?.[0].buffer,
-    ContentType:req.files["businessFile"]?.[0].mimetype
-   }
-   if(businessImageName){
-    const command2 = new PutObjectCommand(params2)
-    await s3.send(command2)
-   }
-  
+    Body: req.files["businessFile"]?.[0].buffer,
+    ContentType: req.files["businessFile"]?.[0].mimetype,
+  };
+  if (businessImageName) {
+    const command2 = new PutObjectCommand(params2);
+    await s3.send(command2);
+  }
 
-   const params3 = {
+  const params3 = {
     Bucket: BUCKET_NAME,
     Key: passportImageName,
-    Body:req.files["passportFile"]?.[0].buffer,
-    ContentType:req.files["passportFile"]?.[0].mimetype
-   }
-   const command3 = new PutObjectCommand(params3)
-   await s3.send(command3)
+    Body: req.files["passportFile"]?.[0].buffer,
+    ContentType: req.files["passportFile"]?.[0].mimetype,
+  };
+  const command3 = new PutObjectCommand(params3);
+  await s3.send(command3);
 
   con.query(
     "SELECT * from otherdetails where id =?",
@@ -641,7 +651,6 @@ app.put("/otherDetails", upload.fields(uploadFields), async(req, res) => {
   );
   const sql =
     "UPDATE otherdetails SET street=?,village=?,addresscountry=?,state=?,postal=?,fatherName=?,fatherNation=?,fatherBirth=?,fatherCountry=?,motherName=?,motherNation=?,motherBirth=?,motherCountry=?,martialStatus=?,spouseName=?,spouseAddress=?,spouseNation=?,spousePlace=?,spouseCountry=?,spouseOccupation=?,spousePhone=?,defenceOrganization=?,defenceDesignation=?,defenceRank=?,defencePosting=?,viAddress=?,viPreviousCity=?,viCountry=?,viVisa=?,viPlaceIssue=?,viDateIssue=?,extendedControlNo=?,extendedDate=?,Q1Detail=?,Q2Detail=?,Q3Detail=?,Q4Detail=?,Q5Detail=?,Q6Detail=?,applicantFile=?,passportFile=?,Aoccupation=?,Q7Detail=?,employerAddress=?,employerName=?,FI_address=?,FI_phone=?,FI_referencename=?,FO_address=?,FO_phone=?,FO_referencename=?,AB_address=?,AB_name=?,AB_phone=?,AB_website=?,IB_address=?,IB_name=?,IB_phone=?,IB_website=?,businessFile=?,typeApplicant=?,typePassport=?,typeBusiness=?,F_placetoVisited=? WHERE id=?";
-  
 
   const values = [
     req.body.street,
@@ -712,7 +721,10 @@ app.put("/otherDetails", upload.fields(uploadFields), async(req, res) => {
   ];
 
   con.query(sql, values, (err, result) => {
-    if (err) console.log(err);
+    if (err) {
+      setTimeout(handleDisconnect, 1000);
+      handleDisconnect();
+    }
     if (result) {
       var mailOptions = {
         from: "info@indiaevisaservices.org",
